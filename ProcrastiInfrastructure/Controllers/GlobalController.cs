0d3f299db3eff14d70936a567696b2a4bec0c562
IvanTsiak <ivanzjak2007@gmail.com>
2026-03-03 18:49:58 +0200

    implement global page

---
+++ ProcrastiInfrastructure/Controllers/GlobalController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.EntityFrameworkCore;
+using ProcrastiDomain.Model;
+using ProcrastiInfrastructure.Models;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProcrastiInfrastructure.Controllers
+{
+    public class GlobalController : Controller
+    {
+        private readonly ProcrastiContext _context;
+
+        public GlobalController(ProcrastiContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var viewModel = new GlobalStatsViewModel();
+
+            viewModel.TopComments = await _context.Logs
+                .Include(l => l.User)
+                .Include(l => l.Activity)
+                .Where(l => !string.IsNullOrEmpty(l.Comment) && l.Isvisible == true)
+                .OrderByDescending(l => l.Likescount)
+                .Take(5)
+                .ToListAsync();
+
+            viewModel.TopUsers = await _context.Users
+                .OrderByDescending(u => u.Totalloss)
+                .Take(3)
+                .ToListAsync();
+
+            viewModel.TopActivities = await _context.Activities
+                .OrderByDescending(a => a.Mentionscount)
+                .Take(3)
+                .ToListAsync();
+
+            return View(viewModel);
+        }
+    }
+}
---
+++ ProcrastiInfrastructure/Models/GlobalStatsViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using ProcrastiDomain.Model;
+
+namespace ProcrastiInfrastructure.Models
+{
+    public class GlobalStatsViewModel
+    {
+        public List<Log> TopComments { get; set; }
+        public List<User> TopUsers { get; set; }
+        public List<Activity> TopActivities { get; set; }
+    }
+}
