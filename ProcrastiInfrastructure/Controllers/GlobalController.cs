554c72017070bafab2c03a2ccc09bc9f2e5011c4
IvanTsiak <ivanzjak2007@gmail.com>
2026-03-21 23:57:30 +0200

    Add basic auth

--- ProcrastiInfrastructure/Controllers/GlobalController.cs
+++ ProcrastiInfrastructure/Controllers/GlobalController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.EntityFrameworkCore;
 using ProcrastiDomain.Model;
@@ -8,6 +9,7 @@ using System.Threading.Tasks;
 
 namespace ProcrastiInfrastructure.Controllers
 {
+    [Authorize]
     public class GlobalController : Controller
     {
         private readonly ProcrastiContext _context;
---
+++ ProcrastiInfrastructure/Services/CurrentUserService.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Security.Claims;
+
+namespace ProcrastiInfrastructure.Services
+{
+    public interface ICurrentUserService
+    {
+        int GetCurrentUserId();
+    }
+
+    public class CurrentUserService : ICurrentUserService
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public CurrentUserService(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        public int GetCurrentUserId()
+        {
+            var user = _httpContextAccessor.HttpContext?.User;
+
+            if (user != null && user.Identity != null && user.Identity.IsAuthenticated)
+            {
+                var userIdClaim = user.FindFirst(ClaimTypes.NameIdentifier);
+
+                if (userIdClaim != null && int.TryParse(userIdClaim.Value, out int userId))
+                {
+                    return userId;
+                }
+            }
+
+            return 0;
+        }
+    }
+}
