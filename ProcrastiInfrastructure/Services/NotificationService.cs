0145ebb5d65629cdd549795d115acdecc3922c2b
IvanTsiak <ivanzjak2007@gmail.com>
2026-04-05 00:04:03 +0300

    implement cleanup notification

--- ProcrastiInfrastructure/Services/NotificationService.cs
+++ ProcrastiInfrastructure/Services/NotificationService.cs
@@ -1,5 +1,7 @@
-﻿using ProcrastiDomain.Model;
+﻿using Microsoft.EntityFrameworkCore;
+using ProcrastiDomain.Model;
 using System;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace ProcrastiInfrastructure.Services
@@ -28,6 +30,36 @@ namespace ProcrastiInfrastructure.Services
 
             _context.Notifications.Add(notification);
             await _context.SaveChangesAsync();
-        }
+
+            bool needsCleanupSave = false;
+
+            var thirtyDaysAgo = DateTime.SpecifyKind(DateTime.UtcNow.AddDays(-30), DateTimeKind.Unspecified);
+            
+            var tooOldNotifications = await _context.Notifications
+                .Where(n => n.UserId == userId && n.CreatedAt < thirtyDaysAgo)
+                .ToListAsync();
+
+            if (tooOldNotifications.Any())
+            {
+                _context.Notifications.RemoveRange(tooOldNotifications);
+                needsCleanupSave = true;
+            }
+
+            var excessNotifications = await _context.Notifications
+                .Where(n => n.UserId == userId)
+                .OrderByDescending(n => n.CreatedAt)
+                .Skip(30)
+                .ToListAsync();
+
+            if (excessNotifications.Any())
+            {
+                _context.Notifications.RemoveRange(excessNotifications);
+                needsCleanupSave = true;
+            }
+
+            if (needsCleanupSave)
+            {
+                await _context.SaveChangesAsync();
+            }
     }
 }
